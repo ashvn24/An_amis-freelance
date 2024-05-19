79d0b48017a61899216b45bb8f712c34bcd92da5
Ashwin vk <133885217+ashvn24@users.noreply.github.com>
2024-05-19 10:06:13 +0530

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import  path
 from .views import *
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
+    path('token/refresh',TokenRefreshView.as_view(),name= 'token_refresh'),
     path('reg/', UserRegAPIView.as_view()),
     path('log/', LoginAPIView.as_view()),
     path('list/', ManageUsersAPIView.as_view()),
